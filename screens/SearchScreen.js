2c9e7cdb3cf4548824861249d9283c6c278eddf0
Shrutarshi Ray <91513676+shrutarshi2002@users.noreply.github.com>
2024-09-07 10:29:32 +0530

    add me

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
-import axios from 'axios';
+import React, { useState } from "react";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
+import axios from "axios";
 
-const SearchScreen = ({ route }) => {
+const SearchScreen = ({ route, navigation }) => {
   const { city } = route.params || {};
-  const [search, setSearch] = useState(city || '');
+  const [search, setSearch] = useState(city || "");
   const [weatherData, setWeatherData] = useState(null);
 
   const fetchWeatherData = async () => {
     try {
-      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather`, {
-        params: {
-          q: search,
-          appid: 'f40af58044177c6613c10d1bbc618659',
-          units: 'metric',
-        },
-      });
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather`,
+        {
+          params: {
+            q: search,
+            appid: "f40af58044177c6613c10d1bbc618659",
+            units: "metric",
+          },
+        }
+      );
       setWeatherData(response.data);
     } catch (error) {
       console.error(error);
@@ -38,6 +41,12 @@ const SearchScreen = ({ route }) => {
           <Text>Temp: {weatherData.main.temp}°C</Text>
           <Text>Weather: {weatherData.weather[0].description}</Text>
           {/* Add more details as needed */}
+          <Button
+            title="View Details"
+            onPress={() =>
+              navigation.navigate("CityDetail", { cityName: weatherData.name })
+            }
+          />
         </View>
       )}
     </View>
@@ -55,7 +64,7 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 20,
     paddingHorizontal: 10,
@@ -65,7 +74,7 @@ const styles = StyleSheet.create({
   },
   city: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
